ffafe730057ea6469215d4ef67b3648ab64edd75
Khoa Nguyen <95058727+Khngu127@users.noreply.github.com>
2024-02-06 19:26:20 -0500

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -1,30 +1,41 @@
+/*
+ * Khoa Nguyen
+ * Affin Malik
+ *
+ *
+ *
+ */
 public class Parser {
-    public static final int OP = 10;      // +  -  *  /
-    public static final int RELOP = 11;   // <  >  <=  >=  ...
-    public static final int LPAREN = 12;  // (
-    public static final int RPAREN = 13;  // )
-    public static final int SEMI = 14;    // ;
-    public static final int COMMA = 15;   // ,
-    public static final int INT = 16;     // int
-    public static final int NUM = 17;     // number
-    public static final int ID = 18;      // identifier
-    public static final int PRINT = 19;   // print
-    public static final int VOID = 20;    // void
-    public static final int FUNC = 21;    // func
-    public static final int IF = 22;      // if
-    public static final int THEN = 23;    // then
-    public static final int ELSE = 24;    // else
-    public static final int WHILE = 25;   // while
-    public static final int BEGIN = 26;   // begin
-    public static final int END = 27;     // end
-    public static final int COLON = 28;   // :
+    public static final int OP = 10;
+    public static final int RELOP = 11;
+    public static final int LPAREN = 12;
+    public static final int RPAREN = 13;
+    public static final int SEMI = 14;
+    public static final int COMMA = 15;
+    public static final int INT = 16;
+    public static final int NUM = 17;
+    public static final int ID = 18;
+    public static final int PRINT = 19;
+    public static final int VOID = 20;
+    public static final int FUNC = 21;
+    public static final int IF = 22;
+    public static final int THEN = 23;
+    public static final int ELSE = 24;
+    public static final int WHILE = 25;
+    public static final int BEGIN = 26;
+    public static final int END = 27;
     public static final int VAR = 29;
     public static final int TYPEOF = 30;
     public static final int ASSIGN = 31;
+    public static final int RELOPGE = 32;
+    public static final int RELOPLE = 33;
+    public static final int RELOPLT = 34;
+    public static final int RELOPEE = 35;
+    public static final int SUCCESS = 36;
 
     private Compiler compiler;
-    private Lexer lexer;       // lexer.yylex() returns token-name
-    public ParserVal yylval;   // yylval contains token-attribute
+    private Lexer lexer;
+    public ParserVal yylval = new ParserVal();
 
     public Parser(java.io.Reader r, Compiler compiler) throws Exception {
         this.compiler = compiler;
@@ -33,76 +44,104 @@ public class Parser {
 
     public int yyparse() throws Exception {
         while (true) {
-            int token = lexer.yylex();  // get next token-name
-            Object attr = yylval.obj;   // get token-attribute
-            String tokenname;
+            int token = lexer.yylex();
+            Object attr = yylval.obj;
+            int tokenStartColumn = lexer.getColumn();
 
             switch (token) {
-                case 0:
-                    // EOF is reached
+                case OP:
+                case RELOP:
+                case LPAREN:
+                case RPAREN:
+                case SEMI:
+                case COMMA:
+                case INT:
+                case NUM:
+                case PRINT:
+                case VAR:
+                case FUNC:
+                case IF:
+                case THEN:
+                case ELSE:
+                case WHILE:
+                case VOID:
+                case BEGIN:
+                case END:
+                    System.out.println("<" + getTokenName(token) + ", token-attr:" + attr + ", " + lexer.lineNum + ":" + (tokenStartColumn) + ">");
+                    break;
+                case ASSIGN:
+                    System.out.println("<" + getTokenName(token) + ", token-attr:" + attr + ", " + lexer.lineNum + ":" + (tokenStartColumn) + ">");
+                    break;
+                case ID:
+                    System.out.println("<" + getTokenName(token) + ", token-attr:" + attr + ", " + lexer.lineNum + ":" + (tokenStartColumn + 1) + ">");
+                    break;
+                case TYPEOF:
+                    System.out.println("<TYPEOF, token-attr:" + attr + ", " + lexer.lineNum + ":" + tokenStartColumn + ">");
+                    break;
+                case SUCCESS:
                     System.out.println("Success!");
-                    return 0;
+                    return SUCCESS;
                 case -1:
-                    // lexical error is found
-                    System.out.println("Error! There is a lexical error at " + lexer.lineno + ":" + lexer.getLexemeStartColumn() + ".");
+                    System.out.println("Error! There is a lexical error at " + lexer.lineNum + ":" + tokenStartColumn + ".");
                     return -1;
-                default:
-                    // Print token information
-                    tokenname = getTokenName(token);
-                    System.out.println("<" + tokenname + ", token-attr:\"" + attr + "\", " + lexer.lineno + ":" + lexer.getLexemeStartColumn() + ">");
             }
         }
     }
 
-    private String getTokenName(int token) {
-        // Return the string representation of a token
-        switch (token) {
-            case OP:
-                return "OP";
-            case RELOP:
-                return "RELOP";
-            case LPAREN:
-                return "LPAREN";
-            case RPAREN:
-                return "RPAREN";
-            case SEMI:
-                return "SEMI";
-            case COMMA:
-                return "COMMA";
-            case INT:
-                return "INT";
-            case NUM:
-                return "NUM";
-            case ID:
-                return "ID";
-            case PRINT:
-                return "PRINT";
-            case VOID:
-                return "VOID";
-            case FUNC:
-                return "FUNC";
-            case IF:
-                return "IF";
-            case THEN:
-                return "THEN";
-            case ELSE:
-                return "ELSE";
-            case WHILE:
-                return "WHILE";
-            case BEGIN:
-                return "BEGIN";
-            case END:
-                return "END";
-            case COLON:
-                return "COLON";
-            case VAR:
-                return "VAR";
-            case TYPEOF:
-                return "TYPEOF";
-            case ASSIGN:
-                return "ASSIGN";
-            default:
-                return "UNKNOWN";
+        public String getTokenName ( int token){
+            switch (token) {
+                case OP:
+                    return "OP";
+                case RELOP:
+                    return "RELOP";
+                case LPAREN:
+                    return "LPAREN";
+                case RPAREN:
+                    return "RPAREN";
+                case SEMI:
+                    return "SEMI";
+                case COMMA:
+                    return "COMMA";
+                case INT:
+                    return "INT";
+                case NUM:
+                    return "NUM";
+                case ID:
+                    return "ID";
+                case PRINT:
+                    return "PRINT";
+                case VOID:
+                    return "VOID";
+                case FUNC:
+                    return "FUNC";
+                case IF:
+                    return "IF";
+                case THEN:
+                    return "THEN";
+                case ELSE:
+                    return "ELSE";
+                case WHILE:
+                    return "WHILE";
+                case BEGIN:
+                    return "BEGIN";
+                case END:
+                    return "END";
+                case VAR:
+                    return "VAR";
+                case TYPEOF:
+                    return "TYPEOF";
+                case ASSIGN:
+                    return "ASSIGN";
+                case RELOPGE:
+                    return "RELOP";
+                case RELOPLE:
+                    return "RELOP";
+                case RELOPLT:
+                    return "RELOP";
+                case RELOPEE:
+                    return "RELOP";
+                default:
+                    return "UNKNOWN";
+            }
         }
     }
-}
